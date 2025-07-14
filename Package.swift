e185a51a4f0e6a73ac3fd312072544b3053c05ac
Shammi Didla <sdidla@gmail.com>
2025-07-14 18:42:41 +0200

    Change minimum macOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "xcresultowners",
-    platforms: [.macOS(.v15)],
+    platforms: [.macOS(.v14)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/indexstore-db", revision: "swift-6.1.1-RELEASE")
