e3b01bc5ce1ce915c263c91e25dbdebfbd1256a7
Shammi Didla <sdidla@gmail.com>
2025-07-22 22:17:03 +0200

    Set minimum version to 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -32,8 +32,7 @@ let package = Package(
             name: "Tests",
             dependencies: ["XCResultOwnersCore"]
         )
-    ],
-    swiftLanguageVersions: [.version("6"), .v5]
+    ]
 )
 
 import Foundation
